79d53e53e3c49821e43a2b9d7075568d51ebdacd
super <gscgoon@163.com>
2021-07-11 23:09:05 +0800

    首次提交，将之前整个项目分为后端代码仓库，后台管理系统（前端），前端web系统

---
+++ guli-parent/common/service-base/src/main/java/com/atguigu/serviceBase/handler/GuliException.java
@@ -0,0 +1,31 @@
+package com.atguigu.serviceBase.handler;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 自定义异常类
+ */
+
+/**
+ * @author Super
+ * @date 2020-04-01 14:49
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GuliException extends  RuntimeException{
+
+    /**
+     * 状态码
+     */
+    private Integer code;
+
+    /**
+     * 信息
+     */
+    private  String message;
+}
